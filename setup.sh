1da9840403a002986a0942a9b1397ad20fa838da
Gehad Sayed Ahmed <Gehad.S.Fadl@kasralainy.edu.eg>
2024-11-25 20:59:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 mkdir -p ~/.streamlit/
-
+cat <<EOF > ~/.streamlit/config.toml
 echo "[server]
+maxUploadSize = 1024
 headless = true
 enableCORS = true
 enableXsrfProtection = true
 port = ${PORT}
+EOF
